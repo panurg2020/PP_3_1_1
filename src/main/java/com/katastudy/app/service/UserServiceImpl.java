80d3532b878ad1cf2f0ac4ff183933477bdc3f61
panurg2020 <hoyvam@mail.ru>
2022-08-23 20:41:46 +0300

    +rewrite

--- src/main/java/com/katastudy/app/service/UserServiceImpl.java
+++ src/main/java/com/katastudy/app/service/UserServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import com.katastudy.app.repository.UserRepository;
 import com.katastudy.app.model.User;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.*;
 
@@ -19,27 +20,30 @@ public class UserServiceImpl {
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
+    @Transactional
     public User findUserById(Long userId) {
         Optional<User> userFromDb = userRepository.findById(userId);
         return userFromDb.orElse(new User());
     }
 
+
     public List<User> allUsers() {
         return userRepository.findAll();
     }
 
+    @Transactional
     public boolean saveUser(User user) {
 
         User userFromDB = userRepository.findByUserName(user.getUsername());
         if (userFromDB != null) {
             return true;
         }
-
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         userRepository.save(user);
         return true;
     }
 
+    @Transactional
     public boolean deleteUser(Long userId) {
         if (userRepository.findById(userId).isPresent()) {
             userRepository.deleteById(userId);
@@ -48,6 +52,7 @@ public class UserServiceImpl {
         return false;
     }
 
+    @Transactional
     public void edit(User user) {
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         userRepository.save(user);
